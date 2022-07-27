353da3194ec026888b69d3f975cf5e41b1d2eaa3
89769 <75208228+karan-65@users.noreply.github.com>
2022-07-27 14:51:02 +0530

    files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:expense_manager/transcations.dart';
+import 'package:expense_manager/models/transcations.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -56,9 +56,15 @@ class homescreen extends StatelessWidget {
               // ignore: prefer_const_literals_to_create_immutables
               children: [
                 TextField(
+                  onChanged: (val) {
+                    TitleInput = val;
+                  },
                   decoration: InputDecoration(labelText: "Title"),
                 ),
                 TextField(
+                  onChanged: (val) {
+                    AmountInput = val;
+                  },
                   decoration: InputDecoration(labelText: "Amount"),
                 ),
                 FlatButton(
