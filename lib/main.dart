62a12b995bdbd5d9f3bb45a28ddb19fd34eb4fe9
89769 <75208228+karan-65@users.noreply.github.com>
2022-07-31 19:14:44 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:expense_manager/widgets/user_transcation.dart';
+import 'package:expense_manager/widgets/new_transcation.dart';
+import 'package:expense_manager/widgets/transcation_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_manager/models/transcations.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,13 +18,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class homescreen extends StatelessWidget {
+class homescreen extends StatefulWidget {
+  @override
+  State<homescreen> createState() => _homescreenState();
+}
+
+class _homescreenState extends State<homescreen> {
+  final List<transcations> _transcartion = [
+    transcations(
+        id: "t1",
+        title: "shoes shopping",
+        amount: 45,
+        datetime: DateTime.now()),
+    transcations(
+        id: "t2",
+        title: "clothes shopping",
+        amount: 41,
+        datetime: DateTime.now()),
+  ];
+
+  void _addnewtranscaations(String title, double amount) {
+    final newtx = transcations(
+        amount: amount,
+        id: DateTime.now().toString(),
+        datetime: DateTime.now(),
+        title: title);
+
+    setState(() {
+      _transcartion.add(newtx);
+    });
+  }
+
+  void _startnewtranscation(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTranscation(_addnewtranscaations);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Manager"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () => _startnewtranscation(context),
+              icon: Icon(Icons.add))
+        ],
       ),
       // ignore: prefer_const_literals_to_create_immutables
       body: SingleChildScrollView(
@@ -33,12 +77,12 @@ class homescreen extends StatelessWidget {
               child: Text("chart"),
             ),
           ),
-          UserTranscation(),
+          TranscationList(_transcartion),
         ]),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startnewtranscation(context),
       ),
     );
   }
